abec9126f70c4ffcf5f00ea31398534e3e3e8039
Liz Garrity <liz@Praneeth.localdomain>
2018-11-14 15:08:27 -0800

    Routes to county pages linked on homepage now render html templates for each county. Observable notebook is loaded into the page for each county. For now, the whole notebook loads. Need to figure out how to isolate just the cell I want for each page. Tested all the links and everything is working as it should up to this point.

--- server.py
+++ server.py
@@ -14,470 +14,254 @@ def index():
 def baker_stats():
   """Show statistics for Baker County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("baker.html")
 
 
 @app.route('/benton')
 def benton_stats():
   """Show statistics for Benton County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("benton.html")
 
 
 @app.route('/clackamas')
 def clackamas_stats():
   """Show statistics for Clackamas County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("clackamas.html")
 
 
 @app.route('/clatsop')
 def clatsop_stats():
   """Show statistics for Clatsop County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("clatsop.html")
 
 
 @app.route('/columbia')
 def columbia_stats():
   """Show statistics for Columbia County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("columbia.html")
 
 
 @app.route('/coos')
 def coos_stats():
   """Show statistics for Coos County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("coos.html")
 
 
 @app.route('/crook')
 def crook_stats():
   """Show statistics for Crook County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("crook.html")
 
 
 @app.route('/curry')
 def curry_stats():
   """Show statistics for Curry County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("curry.html")
 
 
 @app.route('/deschutes')
 def deschutes_stats():
   """Show statistics for Deschutes County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("deschutes.html")
 
 
 @app.route('/douglas')
 def douglas_stats():
   """Show statistics for Douglas County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("douglas.html")
 
 
 @app.route('/gilliam')
 def gilliam_stats():
   """Show statistics for Gilliam County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("gilliam.html")
 
 
 @app.route('/grant')
 def grant_stats():
   """Show statistics for Grant County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("grant.html")
 
 
 @app.route('/harney')
 def harney_stats():
   """Show statistics for Harney County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("harney.html")
 
 
 @app.route('/hoodriver')
 def hood_river_stats():
   """Show statistics for Hood River County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("hoodriver.html")
 
 
 @app.route('/jackson')
 def jackson_stats():
   """Show statistics for Jackson County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("jackson.html")
 
 
 @app.route('/jefferson')
 def jefferson_stats():
   """Show statistics for Jefferson County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("jefferson.html")
 
 
 @app.route('/josephine')
 def josephine_stats():
   """Show statistics for Josephine County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("josephine.html")
 
 
 @app.route('/klamath')
 def klamath_stats():
   """Show statistics for Klamath County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("klamath.html")
 
 
 @app.route('/lake')
 def lake_stats():
   """Show statistics for Lake County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("lake.html")
 
 
 @app.route('/lane')
 def lane_stats():
   """Show statistics for Lane County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("lane.html")
 
 
 @app.route('/lincoln')
 def lincoln_stats():
   """Show statistics for Lincoln County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("lincoln.html")
 
 
 @app.route('/linn')
 def linn_stats():
   """Show statistics for Linn County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("linn.html")
 
 
 @app.route('/malheur')
 def malheur_stats():
   """Show statistics for Malheur County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("malheur.html")
 
 
 @app.route('/marion')
 def marion_stats():
   """Show statistics for Marion County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("marion.html")
 
 
 @app.route('/morrow')
 def morrow_stats():
   """Show statistics for Morrow County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("morrow.html")
 
 
 @app.route('/multnomah')
 def multnomah_stats():
   """Show statistics for Multnomah County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("multnomah.html")
 
 
 @app.route('/polk')
 def polk_stats():
   """Show statistics for Polk County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("polk.html")
 
 
 @app.route('/sherman')
 def sherman_stats():
   """Show statistics for Sherman County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("sherman.html")
 
 
 @app.route('/tillamook')
 def tillamook_stats():
   """Show statistics for Tillamook County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("tillamook.html")
 
 
 @app.route('/umatilla')
 def umatilla_stats():
   """Show statistics for Umatilla County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("umatilla.html")
 
 
 @app.route('/union')
 def union_stats():
   """Show statistics for Union County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("union.html")
 
 
 @app.route('/wallowa')
 def wallowa_stats():
   """Show statistics for Wallowa County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("wallowa.html")
 
 
 @app.route('/wasco')
 def wasco_stats():
   """Show statistics for Wasco County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("wasco.html")
 
 
 @app.route('/washington')
 def washington_stats():
   """Show statistics for Washington County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("washington.html")
 
 
 @app.route('/wheeler')
 def wheeler_stats():
   """Show statistics for Wheeler County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("wheeler.html")
 
 
 @app.route('/yamhill')
 def yamhill_stats():
   """Show statistics for Yamhill County"""
 
-  return """
-      <html>
-        <body>
-          <h1>I am the landing page</h1>
-        </body>
-      </html>
-      """
+  return render_template("yamhill.html")
 
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5010)
+    app.run(debug=True, host='0.0.0.0', port=5012)
